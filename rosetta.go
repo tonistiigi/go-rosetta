4a3fb6279ed9cc89c285e1456a210dd1e5579296
Olivier Lemasle <o.lemasle@gmail.com>
2020-10-30 18:37:54 +0100

    Add documentation

--- rosetta.go
+++ rosetta.go
@@ -7,11 +7,14 @@ import (
 	"syscall"
 )
 
+// Enabled returns true if running in a Rosetta Translated Binary, false otherwise.
 func Enabled() bool {
 	v, err := syscall.SysctlUint32("sysctl.proc_translated")
 	return err == nil && v == 1
 }
 
+// NativeArch returns the native architecture, even if binary architecture
+// is emulated by Rosetta.
 func NativeArch() string {
 	if Enabled() && runtime.GOARCH == "amd64" {
 		return "arm64"
